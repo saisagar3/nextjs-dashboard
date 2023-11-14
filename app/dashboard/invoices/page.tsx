18c9f476718efbcebb6ad3ee612afffb903e2981
saisagar3 <sagar.kachibatla@gmail.com>
2023-11-14 16:54:13 +0700

    Added Pagination

--- app/dashboard/invoices/page.tsx
+++ app/dashboard/invoices/page.tsx
@@ -5,9 +5,10 @@ import Search from "@/app/ui/search";
 import { InvoicesTableSkeleton } from "@/app/ui/skeletons";
 import { Suspense } from "react";
 import Table from "@/app/ui/invoices/table";
+import { fetchInvoicesPages } from "@/app/lib/data";
 
 
-export default function Page({
+export default async function Page({
     searchParams,
 }: {
     searchParams?: {
@@ -17,6 +18,7 @@ export default function Page({
 }) {
     const query = searchParams?.query || '';
     const currentPage = Number(searchParams?.page) || 1;
+    const totalPages = await fetchInvoicesPages(query);
     return (
         <div className="w-full">
             <div className="flex w-full items-center justify-between">
@@ -30,7 +32,7 @@ export default function Page({
                 <Table query={query} currentPage={currentPage} />
             </Suspense>
             <div className="mt-5 flex w-full justify-center">
-                {/* <Pagination totalPages={totalPages} /> */}
+                <Pagination totalPages={totalPages} />
             </div>
         </div>
     )
